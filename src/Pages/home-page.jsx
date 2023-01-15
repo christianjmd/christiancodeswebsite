bfc600c8ed9a19c8a0dc7892975631f79b3503fe
Christian Dinglasan <cdinglasan@deloitte.com.au>
2023-01-16 01:03:07 +1100

    home page content updates

--- src/Pages/home-page.jsx
+++ src/Pages/home-page.jsx
@@ -3,15 +3,14 @@ import React from 'react';
 function Home() {
     return (
         <div><>
-     <h1>Welcome to my website!</h1>
-     <p>My name is Christian and I am a junior software developer.</p>
+     <h1>Christian is a developer and lover of music.</h1>
+     <p>Welcome to my page! As stated by the title I like to code and listen to music.</p>
      <p>
        In this website (which I coded up myself I might add heheh), you can find information about my experience, the work I've
        done, the skills I have, and more.
      </p>
      <p>
-       If you would like to work together, please don't hesitate to{" "}
-       <a href="/contact-page">contact me</a>.
+       If you would like to get in touch, please don't hesitate to <a href="/contact-page">contact me</a>.
      </p>
    </>
     </div>
