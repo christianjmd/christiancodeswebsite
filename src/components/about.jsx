c6b0f21c1de4308f1028a2214638d5680977d5fc
Christian Dinglasan <cdinglasan@deloitte.com.au>
2023-01-15 23:20:16 +1100

    initial commit

---
+++ src/components/about.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+
+render() {
+  return (
+     <div><>
+     <h1>Welcome to my website!</h1>
+     <p>My name is Christian and I am a junior software developer.</p>
+     <p>
+       In this website, you can find information about my experience, the work I've
+       done, the skills I have, and more.
+     </p>
+     <p>
+       If you would like to work together, please don't hesitate to{" "}
+       <a href="">contact me</a>.
+     </p>
+   </>
+    </div>
+  )
+ }
+}
---
+++ src/components/contact.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+export default class Contact extends Component {
+
+render() {
+  return (
+     <div><>
+     <h1>Let's get in touch!</h1>
+     <p>Email: cj.dinglasan@gmail.com</p>
+     <p>Mobile: 0410 218 512</p>
+     <p>Linkedin: https://www.linkedin.com/in/cjmd/</p>
+   </></div>
+  )
+ }
+}
